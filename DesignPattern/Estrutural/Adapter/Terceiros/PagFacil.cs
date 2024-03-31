30f0b1bea7f869955eb19d4d11c1a58b9083fada
Rafael Miranda <rafael.miranda.ext@rendimento.com.br>
2024-03-31 20:55:55 -0300

    Adapter

---
+++ DesignPattern/Estrutural/Adapter/Terceiros/PagFacil.cs
@@ -0,0 +1,47 @@
+﻿namespace DesignPattern.Estrutural.Adapter.Terceiros
+{
+    public class PagFacil
+    {
+        private float _valor;
+        private int _parcelas;
+        private string _numeroCartao;
+        private string _cvv;
+
+        public void Valor(float valor)
+        {
+            _valor = valor;
+        }
+
+        public void Parcelas(int parcelas)
+        {
+            _parcelas = parcelas;
+        }
+
+        public void NumeroCartao(string numeroCartao)
+        {
+            _numeroCartao = numeroCartao;
+        }
+
+        public void Cvv(string cvv)
+        {
+            _cvv = cvv;
+        }
+
+        public bool ValidarCartao()
+        {
+            if (_numeroCartao != string.Empty &&
+               _cvv != string.Empty &&
+               _cvv.Length == 3)
+                return true;
+            else
+                return false;
+        }
+
+        public bool RealizarPagamento()
+        {
+            Console.WriteLine("Pagamento realizado via PagFacil");
+
+            return true;
+        }
+    }
+}
---
+++ DesignPattern/Estrutural/Adapter/Terceiros/TopPagamentos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Estrutural.Adapter.Terceiros
+{
+    public class TopPagamentos
+    {
+        private float _valor;
+        private int _parcelas;
+        private string _numeroCartao;
+        private string _cvv;
+
+        public void ValorTotal(float valor)
+        {
+            _valor = valor;
+        }
+
+        public void QuantidadeParcelas(int parcelas)
+        {
+            _parcelas = parcelas;
+        }
+
+        public void Cartao(string numeroCartao, string cvv)
+        {
+            _numeroCartao = numeroCartao;
+            _cvv = cvv;
+        }
+
+        public bool RealizarPagamento()
+        {
+            Console.WriteLine("Pagamento realizado via TopPagamentos");
+
+            return true;
+        }
+    }
+}
